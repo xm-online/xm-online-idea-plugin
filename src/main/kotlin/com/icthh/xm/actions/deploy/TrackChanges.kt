d00b734f9f06fdc49a54d9af7c65d72d8e5fa8eb
ssenko <sergeysenja1992@gmail.com>
2019-10-27 20:31:07 +0200

    implment index all files

--- src/main/kotlin/com/icthh/xm/actions/deploy/TrackChanges.kt
+++ src/main/kotlin/com/icthh/xm/actions/deploy/TrackChanges.kt
@@ -3,9 +3,13 @@ package com.icthh.xm.actions.deploy
 import com.icthh.xm.utils.getSettings
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-
 import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.util.io.inputStream
+import com.intellij.util.io.isDirectory
+import com.intellij.util.io.systemIndependentPath
 import org.apache.commons.codec.digest.DigestUtils
+import java.nio.file.Files.walk
+import java.nio.file.Paths
 
 
 class TrackChanges() : AnAction() {
@@ -13,7 +17,16 @@ class TrackChanges() : AnAction() {
     override fun actionPerformed(anActionEvent: AnActionEvent) {
         val project = anActionEvent.project
         project ?: return
-        project.getSettings()?.selected()?.trackChanges = true
+        val settings = project.getSettings()?.selected()
+        settings ?: return
+        settings.trackChanges = true
+
+        val basePath = project.basePath + "/config"
+        walk(Paths.get(basePath)).use{
+            it.filter { !it.isDirectory() }.forEach{
+                settings.editedFiles.put(it.systemIndependentPath, DigestUtils.sha256Hex(it.inputStream()))
+            }
+        }
     }
 
     override fun update(anActionEvent: AnActionEvent) {
@@ -26,8 +39,8 @@ class TrackChanges() : AnAction() {
         if (settings != null && settings.trackChanges) {
             val vFile = anActionEvent.getData(PlatformDataKeys.VIRTUAL_FILE)
             val editedFiles = settings.editedFiles
-            if (vFile != null && !editedFiles.contains(vFile.name)) {
-                editedFiles.put(vFile.getName(), DigestUtils.sha256Hex(vFile.inputStream))
+            if (vFile != null && !vFile.isDirectory && !editedFiles.contains(vFile.path)) {
+                editedFiles.put(vFile.path, DigestUtils.sha256Hex(vFile.inputStream))
             }
         }
 
