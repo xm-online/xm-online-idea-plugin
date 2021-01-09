4ff16c517901e0962834c046b8b52ce671d5dcf3
Serhii Senko <serhii.senko@jevera.software>
2021-01-09 13:36:02 +0200

    add ts files

---
+++ src/main/webapp/src/app/role-matrix/role-matrix.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-role-matrix',
+  templateUrl: './role-matrix.component.html',
+  styleUrls: ['./role-matrix.component.css']
+})
+export class RoleMatrixComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
