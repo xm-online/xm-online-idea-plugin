1e2986275acdb29fa6824b6881d14f673b9b7bc0
Serhii Senko <serhii.senko@jevera.software>
2021-07-25 14:42:20 +0300

    fix lep context autocomplete

--- src/main/kotlin/com/icthh/xm/extensions/LepContextNonCodeMembersContributor.kt
+++ src/main/kotlin/com/icthh/xm/extensions/LepContextNonCodeMembersContributor.kt
@@ -3,6 +3,7 @@ package com.icthh.xm.extensions
 
 import com.icthh.xm.extensions.entityspec.originalFile
 import com.icthh.xm.service.*
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiType
@@ -12,6 +13,9 @@ import com.intellij.psi.scope.NameHint
 import com.intellij.psi.scope.PsiScopeProcessor
 import com.intellij.psi.search.ProjectScope
 import com.intellij.psi.search.PsiShortNamesCache
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.GrTypeDefinition
 import org.jetbrains.plugins.groovy.lang.resolve.NonCodeMembersContributor
 
 
@@ -33,12 +37,26 @@ class LepContextNonCodeMembersContributor: NonCodeMembersContributor() {
 
         val nameHint = processor.getHint(NameHint.KEY)
         if (aClass != null && aClass.getCountSubstring() > 0 && nameHint?.getName(state) == "lepContext") {
-            val candidates = PsiShortNamesCache.getInstance(project).getClassesByName("LepContext", ProjectScope.getProjectScope(project))
-            if (candidates.size > 0) {
-                candidates[0].qualifiedName?.let{
-                    val lepContext = LightFieldBuilder("lepContext", it, aClass)
-                    processor.execute(lepContext, state)
-                }
+            injectLepContext(project, aClass, processor, state)
+        }
+
+        if (aClass != null && nameHint == null && place.parent !is GrReferenceExpression && aClass is GrTypeDefinition) {
+            injectLepContext(project, aClass, processor, state)
+        }
+    }
+
+    private fun injectLepContext(
+        project: Project,
+        aClass: PsiClass,
+        processor: PsiScopeProcessor,
+        state: ResolveState
+    ) {
+        val candidates = PsiShortNamesCache.getInstance(project)
+            .getClassesByName("LepContext", ProjectScope.getProjectScope(project))
+        if (candidates.size > 0) {
+            candidates[0].qualifiedName?.let {
+                val lepContext = LightFieldBuilder("lepContext", it, aClass)
+                processor.execute(lepContext, state)
             }
         }
     }
