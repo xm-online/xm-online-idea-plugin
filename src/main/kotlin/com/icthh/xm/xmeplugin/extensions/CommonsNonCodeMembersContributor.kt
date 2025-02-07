a0bfa993f5284605055319b79544474028d95dc6
ssenko <serhii.senko@jevera.software>
2025-02-07 23:34:57 +0200

    Add commons method signature help

--- src/main/kotlin/com/icthh/xm/xmeplugin/extensions/CommonsNonCodeMembersContributor.kt
+++ src/main/kotlin/com/icthh/xm/xmeplugin/extensions/CommonsNonCodeMembersContributor.kt
@@ -6,8 +6,12 @@ import com.intellij.psi.*
 import com.intellij.psi.impl.light.LightFieldBuilder
 import com.intellij.psi.scope.NameHint
 import com.intellij.psi.scope.PsiScopeProcessor
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFile
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.typedef.members.GrMethod
+import org.jetbrains.plugins.groovy.lang.psi.impl.synthetic.GrLightMethodBuilder
 import org.jetbrains.plugins.groovy.lang.resolve.NonCodeMembersContributor
 import org.jetbrains.plugins.groovy.lang.resolve.sorryCannotKnowElementKind
+
 
 class CommonsNonCodeMembersContributor: NonCodeMembersContributor() {
 
@@ -21,26 +25,26 @@ class CommonsNonCodeMembersContributor: NonCodeMembersContributor() {
         val project = place.project
         val selected = project.getSettings().selected()
         selected ?: return
-        if (!project.isSupportProject() || project.isConfigProject()) {
+        if (!project.isSupportProject()) {
             return
         }
 
         val parent = place.parent
         val commons = "lepContext.commons"
 
-        if (!(aClass != null && aClass.getCountSubstring() > 0)) {
-            return
-        }
+//        if (!(aClass != null && aClass.getCountSubstring() > 0)) {
+//            return
+//        }
 
 
         if (parent != null && place.text.startsWith(commons)) {
             val lepCommons = getLepFolder(project, place)?.children?.find { it.name == "commons" } ?: return
 
             val nameHint = processor.getHint(NameHint.KEY)?.getName(state)
-            if (nameHint != null && place.text.endsWith("." + nameHint)) {
-                var path = place.text.substringAfter(commons + ".")
+            if (nameHint != null && place.text.endsWith(".$nameHint")) {
+                var path = place.text.substringAfter("$commons.")
                 path = path.substringBefore("(")
-                var pathParts = path.split(".")
+                val pathParts = path.split(".")
                 var targetFolder: VirtualFile? = lepCommons
                 val DOLLARS = "$" + "$"
                 pathParts.forEach { pathPart ->
@@ -64,9 +68,30 @@ class CommonsNonCodeMembersContributor: NonCodeMembersContributor() {
         processor: PsiScopeProcessor,
         state: ResolveState
     ) {
-            val param = LightFieldBuilder(name, "java.lang.Object", element)
-            val newState = state.put(sorryCannotKnowElementKind, true)
-            processor.execute(param, newState)
+        if (element is GroovyFile) {
+            val commonsName = element.name.substringAfter("Commons$$").substringBefore("$${'$'}tenant").substringBefore("$${'$'}around")
+            val declarationMethod = element.scriptClass?.methods?.filterIsInstance<GrMethod>()?.find {
+                "signatureDeclaration" == it.name || commonsName == it.name
+            }
+            if (declarationMethod != null) {
+                declarationMethod.let {
+                    val method = GrLightMethodBuilder(element.manager, name)
+                    method.returnType = it.returnType
+                    method.setNamedParameters(it.namedParameters)
+                    it.parameters.forEach { param ->
+                        method.addParameter(param.name, param.type)
+                    }
+                    method.navigationElement = it
+                    val newState = state.put(sorryCannotKnowElementKind, true)
+                    processor.execute(method, newState)
+                    return
+                }
+            }
+        }
+
+        val param = LightFieldBuilder(name, "java.lang.Object", element)
+        val newState = state.put(sorryCannotKnowElementKind, true)
+        processor.execute(param, newState)
     }
 
 }
